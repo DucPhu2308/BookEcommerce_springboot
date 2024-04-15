c22a5de1ffff990d8c3deb3f2be81f5940d4a423
DucPhu2308 <nguyenducphu2308@gmail.com>
2024-04-15 11:41:12 +0700

    add CrossOrigin for AuthCon

--- src/main/java/hcmute/leettruyen/controller/AuthController.java
+++ src/main/java/hcmute/leettruyen/controller/AuthController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class AuthController {
     private final IUserService userService;
