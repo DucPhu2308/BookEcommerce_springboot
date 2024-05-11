97aa3b37aa027d3aa23e7034901308ade42da829
kow111 <chome710@gmail.com>
2024-05-12 00:09:15 +0700

    minor fix

--- src/main/java/hcmute/leettruyen/service/IPurchasedService.java
+++ src/main/java/hcmute/leettruyen/service/IPurchasedService.java
@@ -4,4 +4,5 @@ import hcmute.leettruyen.entity.PurchasedHistory;
 import java.util.List;
 public interface IPurchasedService {
     List<PurchasedHistory> findByCrtUser() throws Exception;
+    boolean checkPurchased(Integer bookId) throws Exception;
 }
--- src/main/java/hcmute/leettruyen/service/implement/PurchasedImpl.java
+++ src/main/java/hcmute/leettruyen/service/implement/PurchasedImpl.java
@@ -1,8 +1,10 @@
 package hcmute.leettruyen.service.implement;
 
 import hcmute.leettruyen.component.Extractor;
+import hcmute.leettruyen.entity.Book;
 import hcmute.leettruyen.entity.PurchasedHistory;
 import hcmute.leettruyen.entity.User;
+import hcmute.leettruyen.repository.BookRepository;
 import hcmute.leettruyen.repository.PurchasedHistoryRepository;
 import hcmute.leettruyen.repository.UserRepository;
 import hcmute.leettruyen.service.IPurchasedService;
@@ -16,6 +18,7 @@ import java.util.List;
 @Service
 public class PurchasedImpl implements IPurchasedService {
     private final PurchasedHistoryRepository purchasedHistoryRepository;
+    private final BookRepository bookRepository;
     private final Extractor extractor;
     private final UserRepository userRepository;
     @Override
@@ -23,5 +26,18 @@ public class PurchasedImpl implements IPurchasedService {
         User user = userRepository.findById(extractor.getUserIdFromToken())
                 .orElseThrow(()-> new Exception("Cannot find user"));
         return purchasedHistoryRepository.findByUser(user);
+    }
+
+    @Override
+    public boolean checkPurchased(Integer bookId) throws Exception {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(()-> new Exception("Cannot find book"));
+        List<PurchasedHistory> purchasedHistories = findByCrtUser();
+        for (PurchasedHistory purchasedHistory : purchasedHistories){
+            if(purchasedHistory.getChapter().getBook().equals(book)){
+                return true;
+            }
+        }
+        return false;
     }
 }
