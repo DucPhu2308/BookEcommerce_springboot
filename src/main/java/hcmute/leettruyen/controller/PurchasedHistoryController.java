5d40876cc5f2b3eb02488da099b796768f189de8
kow111 <chome710@gmail.com>
2024-05-11 18:21:38 +0700

    minor fix

--- src/main/java/hcmute/leettruyen/controller/PurchasedHistoryController.java
+++ src/main/java/hcmute/leettruyen/controller/PurchasedHistoryController.java
@@ -1,10 +1,15 @@
 package hcmute.leettruyen.controller;
 
+import hcmute.leettruyen.entity.PurchasedHistory;
 import hcmute.leettruyen.entity.ResponseObject;
+import hcmute.leettruyen.response.PurchasedHistoryResponse;
 import hcmute.leettruyen.service.IPurchasedService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/purchased-history")
@@ -12,12 +17,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PurchasedHistoryController {
     private final IPurchasedService purchasedService;
+    private final ModelMapper modelMapper;
     @GetMapping("/all")
     public ResponseEntity<ResponseObject> getPurchasedHistory(){
         try {
+            List<PurchasedHistory> purchasedHistories = purchasedService.findByCrtUser();
+
             return ResponseEntity.ok(
                     new ResponseObject("ok",
-                            "",purchasedService.findByCrtUser()));
+                            "",purchasedHistories.stream()
+                            .map(purchasedHistory -> modelMapper.map(purchasedHistory, PurchasedHistoryResponse.class))
+                            .toList()));
         }catch (Exception e){
             return ResponseEntity.badRequest().body(
                     new ResponseObject("Fail",e.getMessage(),""));
